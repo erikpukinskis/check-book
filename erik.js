dab98cfec8ba46f4c7e7a7770aa13bddb98d40f4
Erik Pukinskis <erik@snowedin.net>
2017-02-10 20:08:42 -0800

    stuff

--- erik.js
+++ erik.js
@@ -18,7 +18,6 @@ library.using(
         ["Feburary rent", "-$2,085.00", "2/6/2017"],
         ["Mom", "$100.00", "2/6/2017"],
         ["Teensy house gutter", "-7.63", "2/7/2017"],
-        // ["Clipper", "-20", "2/6/2017"],
         ["Sandwich & Coffee", "-17.18", "2/6/2017"],
         ["Lyft to Marie", "-9.29", "2/6/2016"],
       ])
